e712c3b8e5841271e5b5d3ce12eadefaddc3bd7a
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-04-27 16:23:29 -0400

    Finish adding todo item control and modal for form input (#4)
    * Hackathon speed?

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+import styles from '@/App.module.scss';
 import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
-import styles from './App.module.scss';
+
+import TodoControl from './components/TodoControl';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
-    <Stack className={styles.todoItems} gap={3}>
-      <div>First item <Button variant="primary">COMPLETE</Button></div>
-    </Stack>
+    <>
+      <Stack className={styles.header}>
+        <div>Tackle It</div>
+      </Stack>
+      <TodoControl />
+      <TodoItem />
+    </>
   );
 }
 
