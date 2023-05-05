c5b1037b38116d321becaee600c4d358132297f0
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-05-05 18:27:05 -0400

    Test

--- backend/utils/logger.ts
+++ backend/utils/logger.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any */
 
 const info = (...params: any[]) => {
-  if (process.env.NODE_ENV === 'test') {
-    return;
-  }
+  // if (process.env.NODE_ENV === 'test') {
+  //   return;
+  // }
   console.log(...params);
 };
 
 const error = (...params: any[]) => {
-  if (process.env.NODE_ENV === 'test') {
-    return;
-  }
+  // if (process.env.NODE_ENV === 'test') {
+  //   return;
+  // }
   console.error(...params);
 };
 
